9486700650ef0059bcdf81a11cc734a34b39701c
Jun Matsushita <jun@iilab.org>
2015-04-30 11:04:00 +0200

    Long dash typo

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 # This script is meant to be ran under the Git for Windows bash shell command line
 #
 clear
-git config –-system core.autocrlf false
+git config --system core.autocrlf false
 echo ""
 echo "--------------------------------------------------------------------"
 echo ""
